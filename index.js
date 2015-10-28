75d2ee9f500348706622baee0629d0918b67e8b0
Michael Kühnel <m.kuehnel@micromata.de>
2015-10-29 00:18:16 +0100

    Add proof of concept with static »search«

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
+
+var osascript = require('osascript');
+var fs = require('fs');
+
 module.exports = function (str, opts) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
 	opts = opts || {};
+
+// Run JavaScript file through OSA
+fs.createReadStream('lib/play.js')
+	.pipe(osascript())
+	.pipe(process.stdout);
 
 	return str + ' & ' + (opts.postfix || 'rainbows');
 };
