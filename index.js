17da810baaa34412759a1befd20bea7f7d075769
Michael Kühnel <m.kuehnel@micromata.de>
2015-10-29 22:09:31 +0100

    Make it possible to start/stop playback without searching

--- index.js
+++ index.js
@@ -4,11 +4,46 @@ var osascript = require('osascript').eval;
 var stringify = require('js-function-string');
 var search = require('./lib/search');
 var play = require('./lib/play');
+var pause = require('./lib/pause');
+var stop = require('./lib/stop');
 var logSymbols = require('log-symbols');
 var chalk = require('chalk');
 var isEmptyObject = require('is-empty-object');
 
-function startPlaying(playlist, amount) {
+function startPlayBack() {
+	// Run JavaScript file through OSA
+	osascript(stringify(play.method), function (err) {
+		if (err === null) {
+			console.log(logSymbols.success, 'Playing ♪♬');
+		} else {
+			console.log(logSymbols.error, chalk.red(err));
+		}
+	});
+}
+
+function stopPlayback() {
+	// Run JavaScript file through OSA
+	osascript(stringify(stop.method), function (err) {
+		if (err === null) {
+			console.log(logSymbols.success, 'Stopped playing ♪♬');
+		} else {
+			console.log(logSymbols.error, chalk.red(err));
+		}
+	});
+}
+
+function pausePlayback() {
+	// Run JavaScript file through OSA
+	osascript(stringify(pause.method), function (err) {
+		if (err === null) {
+			console.log(logSymbols.success, 'Paused playing ♪♬');
+		} else {
+			console.log(logSymbols.error, chalk.red(err));
+		}
+	});
+}
+
+function startPlaylist(playlist, amount) {
 	// Run JavaScript file through OSA
 	osascript(stringify(play.method).replace(/{{playlist}}/, playlist), function (err) {
 		if (err === null) {
@@ -29,7 +64,7 @@ function startSearch(searchTerm, opts) {
 			console.log(logSymbols.success, 'Found songs, albums and artists containing ”' +
 				chalk.inverse(searchTerm) + '“ and generated a temporary playlist');
 			if (opts.start) {
-				startPlaying(playlist, amount);
+				startPlaylist(playlist, amount);
 			}
 		} else if (err === null) {
 			console.log(logSymbols.error, 'Oops. Found 0 songs, albums and artists containing ”' +
@@ -45,16 +80,28 @@ module.exports = function (str, opts) {
 		throw new TypeError('Expected a string');
 	}
 
-	if (isEmptyObject(opts)) {
-		opts = {start: true};
-	}
-
 	if (str) {
+		if (isEmptyObject(opts)) {
+			opts = {start: true};
+		}
 		startSearch(str, opts);
 	} else {
-		return logSymbols.warning +
-			' Please enter something you like to listen to.\n' +
-			chalk.yellow('Enter `itunes-remote --help` for details');
+		if (isEmptyObject(opts)) {
+			return logSymbols.warning +
+				' Please enter something you like to listen to.\n' +
+				chalk.yellow('Enter `itunes-remote --help` for details');
+		} else {
+			console.log(opts);
+			if (opts.start === true) {
+				startPlayBack();
+			}
+			if (opts.stop === true) {
+				stopPlayback();
+			}
+			if (opts.pause === true) {
+				pausePlayback();
+			}
+		}
 	}
 
 	return 'Hold on …';
