e7f08344c735814b2dc25894982c6c7ca2666c9f
Michael Kühnel <m.kuehnel@micromata.de>
2015-10-29 22:23:26 +0100

    Add first test

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ var itunesRemote = require('./');
 
 
 describe("itunes-remote", function() {
-	it("returns correct string", function() {
-		expect(itunesRemote('unicorns', {postfix: 'foo'})).to.equal('Hold on …');
+	it("should return a hint when called without searchterm and options", function() {
+		// expect(itunesRemote('unicorns', {postfix: 'foo'})).to.equal('Hold on …');
+		expect(itunesRemote('', {})).to.equal('\u001b[33m⚠\u001b[39m Please enter something you like to listen to.\n\u001b[33mEnter `itunes-remote --help` for details\u001b[39m');
 	});
 });
