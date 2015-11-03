f13c8b7b62bb567ca29fc5b0aabdd18f25abaac1
Michael Kühnel <m.kuehnel@micromata.de>
2015-11-04 00:57:35 +0100

    Fix linting errors

--- index.js
+++ index.js
@@ -12,44 +12,52 @@ var chalk = require('chalk');
 function startPlayback(callback) {
 	// Run JavaScript file through OSA
 	osascript(stringify(play.method), function (err) {
+		var result;
 		if (err === null) {
-			return callback(logSymbols.success + ' Playing ♪♬');
+			result = callback(logSymbols.success + ' Playing ♪♬');
 		} else {
-			return callback(logSymbols.error + ' ' + chalk.red(err));
+			result = callback(logSymbols.error + ' ' + chalk.red(err));
 		}
+		return result;
 	});
 }
 
 function stopPlayback(callback) {
 	// Run JavaScript file through OSA
 	osascript(stringify(stop.method), function (err) {
+		var result;
 		if (err === null) {
-			return callback(logSymbols.success + ' Stopped playing ♪♬');
+			result = callback(logSymbols.success + ' Stopped playing ♪♬');
 		} else {
-			return callback(logSymbols.error + ' ' + chalk.red(err));
+			result = callback(logSymbols.error + ' ' + chalk.red(err));
 		}
+		return result;
 	});
 }
 
 function pausePlayback(callback) {
 	// Run JavaScript file through OSA
 	osascript(stringify(pause.method), function (err) {
+		var result;
 		if (err === null) {
-			return callback(logSymbols.success + ' Paused playing ♪♬');
+			result = callback(logSymbols.success + ' Paused playing ♪♬');
 		} else {
-			return callback(logSymbols.error + ' ' + chalk.red(err));
+			result = callback(logSymbols.error + ' ' + chalk.red(err));
 		}
+		return result;
 	});
 }
 
 function startPlaylist(playlist, amount, callback) {
 	// Run JavaScript file through OSA
 	osascript(stringify(play.method).replace(/{{playlist}}/, playlist), function (err) {
+		var result;
 		if (err === null) {
-			return callback(logSymbols.success + ' Playing ' + amount + ' song(s) ♪♬');
+			result = callback(logSymbols.success + ' Playing ' + amount + ' song(s) ♪♬');
 		} else {
-			return callback(logSymbols.error + ' ' + chalk.red(err));
+			result = callback(logSymbols.error + ' ' + chalk.red(err));
 		}
+		return result;
 	});
 }
 
@@ -59,47 +67,49 @@ function startSearch(searchTerm, opts, callback) {
 		data = data.split(',');
 		var playlist = data[0];
 		var amount = parseInt(data[1], 10);
+		var result;
 		if (err === null && amount) {
 			if (!opts.dontplay) {
-				startPlaylist(playlist, amount, function(response) {
-					return callback(response);
+				startPlaylist(playlist, amount, function (response) {
+					result = callback(response);
 				});
 			}
-			return callback(logSymbols.success + ' Found songs, albums and artists containing ”' +
+			result = callback(logSymbols.success + ' Found songs, albums and artists containing ”' +
 				chalk.inverse(searchTerm) + '“ and generated a temporary playlist');
 		} else if (err === null) {
-			return callback(logSymbols.error + ' Oops. Found 0 songs, albums and artists containing ”' +
+			result = callback(logSymbols.error + ' Oops. Found 0 songs, albums and artists containing ”' +
 				chalk.inverse(searchTerm) + '“.');
 		} else {
-			return callback(logSymbols.error + ' ' + chalk.red(err));
+			result = callback(logSymbols.error + ' ' + chalk.red(err));
 		}
+		return result;
 	});
 }
 
 module.exports = function (command, callback, args) {
-
 	switch (command) {
 		case 'play':
-			startPlayback(function(response) {
+			startPlayback(function (response) {
 				return callback(response);
 			});
 			break;
 		case 'stop':
-			stopPlayback(function(response) {
+			stopPlayback(function (response) {
 				return callback(response);
 			});
 			break;
 		case 'pause':
-			pausePlayback(function(response) {
+			pausePlayback(function (response) {
 				return callback(response);
 			});
 			break;
 		case 'search':
 			// console.log(args);
-			startSearch(args.searchterm, args.options, function(response) {
+			startSearch(args.searchterm, args.options, function (response) {
 				return callback(response);
 			});
 			break;
+		default:
+			throw new Error('You have to call itunesRemote with a command');
 	}
-
 };
--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 var expect = require('chai').expect;
 var itunesRemote = require('./');
 
-describe('itunes-remote', function () {
-	it('should return a hint when called without searchterm and options', function () {
-		// expect(itunesRemote('unicorns', {postfix: 'foo'})).to.equal('Hold on …');
-		expect(itunesRemote('', {})).to.equal('\u001b[33m⚠\u001b[39m Please enter something you like to listen to.\n\u001b[33mEnter `itunes-remote --help` for details\u001b[39m');
+describe('itunesRemote', function () {
+	it('should throw an error when called without command', function () {
+		expect(itunesRemote).to.throw(Error, /You have to call itunesRemote with a command/);
 	});
 });
